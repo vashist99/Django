dc297d78d1cfa6a63d47538931eb7fba842d9e7c
vashist_99 <201752040@iiitvadodara.ac.in>
2018-05-16 19:45:45 +0530

    this is where i have rendered simple html templates

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse('hello world!')
+    return render(request,'homepage.html')
 
 def about(request):
-    return HttpResponse('this is the about page')
+    return render(request,'about.html')
