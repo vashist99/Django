3c234e93c85525ff2638fe1e8570633e98f47aad
vashist_99 <201752040@iiitvadodara.ac.in>
2018-05-16 21:22:50 +0530

    i have implemented the firs django maodel

---
+++ articles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request,'articles\homepage.html')
+
+def about(request):
+    return render(request,'articles\ about.html')    
+
+# Create your views here.
