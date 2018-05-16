03bf3ded86ac0f7ade14bd3eaeebd9d0af7ef6ed
vashist_99 <201752040@iiitvadodara.ac.in>
2018-05-16 20:15:47 +0530

    have created my first django APP

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url,include
 from.import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',views.homepage),
     url(r'^about$',views.about),
+    url(r'^articles',include('articles.urls'))
 ]
