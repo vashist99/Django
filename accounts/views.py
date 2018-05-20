a815f3b03aa6e16fc6358f33429e7f94f6f66c31
vashist_99 <201752040@iiitvadodara.ac.in>
2018-05-20 10:54:42 +0530

    added django forms for login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def main(request):
-    return render(request,'accounts/main.html')
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if formis_valid():
+            form.save()
+            return redirect('articles:articlehomepage')
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/main.html',{'form':form})
